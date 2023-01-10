f74436195fe5d956f920f8f70fec8b77884f0646
Javi <vatojavier@gmail.com>
2023-01-10 12:43:21 +0100

    Changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
 
+# Adding some stuff lmao
 if __name__ == '__main__':
     digits = datasets.load_digits()
 
